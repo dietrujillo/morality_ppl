14c42c073defb5f003d1822509d1b1c09e04818a
Diego Trujillo <diegotrujillojimenez@hms.harvard.edu>
2023-12-05 13:41:33 -0500

    Add damage value estimation

--- generative_model.jl
+++ generative_model.jl
@@ -1,6 +1,37 @@
 module GenerativeModel
 
 using Gen
+using CSV
+using DataFrames
+using StatsBase: iqr, median
+
+const DamageType = Symbol
+const VALID_DAMAGE_TYPES::Vector{DamageType} = [
+    :bluemailbox,
+    :blueoutsidedoor,
+    :bluehouse,
+    :cuttree,
+    :breakwindows,
+    :razehouse,
+    :bleachlawn, 
+    :blueinsidedoor,
+    :erasemural,
+    :smearpoop
+]
+
+_combine_median(df::DataFrame)::DataFrameRow = combine(df, names(df) .=> median, renamecols=false)[1, :]
+_combine_iqr(df::DataFrame)::DataFrameRow = combine(df, names(df) .=> iqr, renamecols=false)[1, :]
+function _build_damage_table(compensation_demanded::DataFrame)::DataFrame
+    out = DataFrame()
+    push!(out, _combine_median(compensation_demanded))
+    push!(out, _combine_iqr(compensation_demanded))
+    return out
+end
+
+const COMPENSATION_DEMANDED_FILEPATH = "data/data_wide_willing.csv"
+const COMPENSATION_DEMANDED_TABLE = _build_damage_table(
+    CSV.read(COMPENSATION_DEMANDED_FILEPATH, DataFrame)[:, VALID_DAMAGE_TYPES]
+)
 
 function _kahneman_tversky_utility(x::Real, α::Real = 0.25; λ::Real = 2.25)::Real
     if x < 0
@@ -10,6 +41,16 @@ function _kahneman_tversky_utility(x::Real, α::Real = 0.25; λ::Real = 2.25)::R
 end
 
 logistic(x::Real) = inv(exp(-x) + one(x))
+
+@gen function estimate_damage_value(damage_type::DamageType)
+    median, iqr = COMPENSATION_DEMANDED_TABLE[:, damage_type]
+    damage_value ~ HomogeneousMixture(normal, [0, 0])(
+        dirichlet([1., 1., 1.]),
+        [normal(median, 1), normal(median, 1), normal(median, 1)],
+        [normal(iqr, 1), normal(iqr, 1), normal(iqr, 1)]
+    )
+    return damage_value
+end
 
 @gen function estimate_side_payment_fraction(amount_offered::Float64)
     fraction ~ HomogeneousMixture(normal, [0, 0])(
@@ -26,7 +67,7 @@ end
     return multiplier
 end
 
-@gen function model_acceptance(amounts_offered::Vector{Float64}, damage_types::Vector{Float64})
+@gen function model_acceptance(amounts_offered::Vector{Float64}, damage_types::Vector{DamageType})
 
     min_utility_threshold ~ uniform(0, 10)
     max_cost_threshold ~ uniform(1000, 100000)
@@ -46,7 +87,7 @@ end
     end
 
     for (i, (amount_offered, damage_type)) in enumerate(zip(amounts_offered, damage_types))
-        damage_amount = damage_type#({(:damage_amount, i)} ~ estimate_damage_amount(damage_type))
+        damage_amount = ({(:damage_value, i)} ~ estimate_damage_value(damage_type))
         side_payment_fraction = {(i, :side_payment_fraction)} ~ estimate_side_payment_fraction(amount_offered)
         money_amount = amount_offered * min(1, max(0, side_payment_fraction))
         {(i, :acceptance)} ~ bernoulli(accept_probability(money_amount, damage_amount))
--- moral_ppl.jl
+++ moral_ppl.jl
@@ -8,8 +8,20 @@ using .GenerativeModel: model_acceptance
 
 export main, VALID_DAMAGE_TYPES, COMPENSATION_DEMANDED_TABLE
 
+function model_inference(model, data)
+    
+    observations = Gen.choicemap()
+    for (i, y) in enumerate(data[:bargain_accepted])
+        observations[(i, :acceptance)] = y
+    end
+    
+    num_samples = 10000
+    (trace, _) = Gen.importance_resampling(model, (data[:amount_offered], data[:damage_type]), observations, num_samples);
+    return trace
+end
+
 function main()
-    choicemap = Gen.get_choices(Gen.simulate(model_acceptance, ([10000., 1001.], [100., 900.])))
+    choicemap = Gen.get_choices(Gen.simulate(model_acceptance, ([10000., 1001.], [:razehouse, :bluemailbox])))
     return choicemap
 end
 
