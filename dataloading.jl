134a4e1be0b09423c5b4dfeec68410a6759f81ab
Mindy Zhang <mindyz@mit.edu>
2024-02-14 19:05:59 -0500

    data split now trackes and splits by response id

--- dataloading.jl
+++ dataloading.jl
@@ -14,10 +14,27 @@ const OFFER_AS_INT_DICT = Dict(
     "million" => 1000000,
 )
 
-function splitdf(df, pct)
+function splitdf(df, pct, individual_analysis::Bool = True)
     @assert 0 <= pct <= 1
-    ids = shuffle(collect(axes(df, 1)))
-    sel = ids .<= nrow(df) .* pct
+
+    if individual_analysis
+        n = Int(pct*10)
+        sel = Vector{Bool}(undef, nrow(df))
+        fill!(sel, false)
+        all_response_ids = [responseid for responseid in unique(table, [:responseID])[!,:responseID]] #305 total responders
+        for responder in all_response_ids
+        train_types = sample(VALID_DAMAGE_TYPES, n; replace=false)
+        for damage in train_types
+            indxs = findall((df.damage_type .== damage) .& (df.responseID .== responder))
+            for indx in indxs
+            sel[indx] = 1 
+            end
+        end
+        end
+    else
+        ids = shuffle(collect(axes(df, 1)))
+        sel = ids .<= nrow(df) .* pct
+    end
     return view(df, sel, :), view(df, .!sel, :)
 end
 
@@ -26,15 +43,15 @@ function load_dataset(data_path::String)
     table = stack(table, VALID_DAMAGE_TYPES)
     rename!(table, [:variable, :value] .=> [:damage_type, :bargain_accepted])
     table = filter(row -> row[:condition] in keys(OFFER_AS_INT_DICT), table)
-
+ 
     table[:, :amount_offered] = map(x -> OFFER_AS_INT_DICT[x], table[:, :condition])
 
-
+    table[!,:responseID] = convert.(String, table[:,:responseID])
     table[!,:damage_type] = Symbol.(table[:,:damage_type])
     table[!,:amount_offered] = convert.(Float64, table[:,:amount_offered])
     table[!,:bargain_accepted] = convert.(Bool, table[:,:bargain_accepted])
 
-    return table[:, [:damage_type, :amount_offered, :bargain_accepted]]
+    return table[:, [:responseID, :damage_type, :amount_offered, :bargain_accepted]]
 end
 
 function load_and_split(data_path)
